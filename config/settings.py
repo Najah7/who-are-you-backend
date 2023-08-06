15f1024c4aa213e780db1add244f1a4a48399deb
Najah7 <najahmatsuo7@gmail.com>
2023-08-07 07:31:59 +0900

    🔧 #2 [Add] 画像系の保管場所を設定

--- config/settings.py
+++ config/settings.py
@@ -126,4 +126,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 AUTH_USER_MODEL = "core.User"
